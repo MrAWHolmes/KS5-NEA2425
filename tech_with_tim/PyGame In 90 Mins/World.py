1b6473176440d5c4c8c7f881ee676191e8445b80
MrAWHolmes <archie.holmes@gmail.com>
2024-08-30 22:24:19 +0100

    World and HashQ v0.0

--- tech_with_tim/PyGame In 90 Mins/World.py
+++ tech_with_tim/PyGame In 90 Mins/World.py
@@ -1,72 +1,105 @@
 import pygame
 import os
 
+class HashQ:
+    Q = None
+    X = None
 
+    def __init__(self):
+        self.Q = dict()
+
+    def addNode(self,name,anyObject):
+        if name not in self.Q:
+            self.Q[name] = list()
+        self.Q[name].append(anyObject)
+    
+    def delNode(self,name):
+        if name in self.Q:
+            try:
+                del this.Q[name]
+            except:
+                this.X.append(name)    
+
+
+    
 class World:
+    COLOURS = None
     WIN = None
+    GfxQ = None
+    EventsQ = None
+    FPS = None
 
-    def __init__(self,)
+    def __init__(self,width:int,height:int,title:str,fps:int=60):
+        """
+        Some defualt colour tuples...
+        """
+        World.COLOURS = dict()
+        World.COLOURS["BLACK"] = (0,0,0)
+        World.COLOURS["WHITE"] = (255,255,255)
+        World.COLOURS["RED"] = (255,0,0)
+        World.COLOURS["GREEN"] = (0,255,0)
+        World.COLOURS["BLUE"] = (0,0,255)
+
+        self.WIDTH = width
+        self.HEIGHT = height
+        self.FPS = fps
+        
+        self.WIN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption(title)
+
+        GfxQ = dict()
+        EventsQ = dict()
 
 
-def startPygame():
-    global WIDTH,HEIGHT, WIN, WHITE, BLACK,RED,YELLOW,GfxObjectList
-    pygame.font.init()
-    pygame.mixer.init()
-
-    WIDTH, HEIGHT = 900, 500
-    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("First Game!")
-
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
-    YELLOW = (255, 255, 0)
-
-    gfxObjectList = list()
-
-
-
-
-
-def draw_window(GfxObjectList:list):
-    """
-
-    """
-    WIN.fill(BACK)
-
-    for gfxObject in GfxObjectList:
-        if hasattr(gfxObject,'draw'):
-            gfxObject.draw()
-
-    pygame.display.update()
+        pygame.font.init()
+        pygame.mixer.init()
 
 
 
-def main_loop(FPS:int,GfxObjectList:list):
+    def drawWindow(self):
+        """
 
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(FPS)
+        """
+        self.WIN.fill(self.COLOURS["BLACK"])
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
+        for gfxObject in GfxQ:
+            if hasattr(gfxObject,'draw'):
+                gfxObject.draw()
 
-            if event.type == pygame.KEYDOWN:
+        pygame.display.update()
 
-                for gfxObject in gfxObjectList:
-                    if hasattr(fgxObject,'grabKeydown'):
-                        fgxObject.grabKeydown(event)
+    def joinGfxQ(self,name:str,GfxObj):
+        self.GfxQ[name] = GfxObj
 
-        keys_pressed = pygame.key.get_pressed()
+    def leaveGfxQ(self,name):
+        if not hasattr(self,"exitQ"):
+            self.exitQ = list()
+        self.exitQ.append(name)
 
 
-        draw_window(gfxObjectList)
+    def main_loop(self,FPS:int):
 
-def addGfxObject(gfxObject):
-    global GfxObjectList
+        clock = pygame.time.Clock()
+        run = True
+        while run:
+            clock.tick(FPS)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+
+                if event.type == pygame.KEYDOWN:
+
+                    for gfxObject in gfxObjectList:
+                        if hasattr(fgxObject,'grabKeydown'):
+                            fgxObject.grabKeydown(event)
+
+            keys_pressed = pygame.key.get_pressed()
+
+
+            draw_window(gfxObjectList)
+
 
 
 def main():
